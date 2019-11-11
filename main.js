71bc036f4766c7283d6070e0450d21ceaf40e2bc
João Paulo <joaop.rosa01@gmail.com>
2019-11-11 16:39:47 -0300

    continuação do projeto Netflix

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 function disparar () {
-	if (document.querySelector('input').style.display == 'none'){
-	  document.querySelector('input').style.display = 'flex';
-	  document.querySelector('span').style.border = '0.5px solid #FFFFFF';
-	} else {
+	if (document.querySelector('input').style.display == 'flex'){
 	  document.querySelector('input').style.display = 'none';
 	  document.querySelector('span').style.border = 'none';
+	  document.querySelector ('.menu-2').style.width = '20vw'
+	  
+	} else {
+	  document.querySelector('input').style.display = 'flex';
+	  document.querySelector('span').style.border = '0.5px solid #FFFFFF';
+	  document.querySelector ('.menu-2').style.width = 'calc(20vw + 177px)'
 	}
+}
+
+function test (sessao) {
+	console.log('.' + sessao + '-1')
+	document.querySelector('.' + sessao + '-1').style.display = 'none';
+	document.querySelector('.' + sessao + '-2').style.display = 'flex';
+
+}
+
+function test2 (sessao) {
+	console.log('.' + sessao + '-1')
+	document.querySelector('.' + sessao + '-2').style.display = 'none';
+	document.querySelector('.' + sessao + '-1').style.display = 'flex';
+
 }
