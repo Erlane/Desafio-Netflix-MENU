3d923d040fe8bbf479a103acbf9dc2fe3f264ed9
João Paulo <joaop.rosa01@gmail.com>
2019-11-18 12:47:53 -0300

    continuação do projeto

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
+function mudarCor(event) {
+	console.log('passei e sai correndo')
+	if(window.scrollY <= 0){
+		document.querySelector('.menu').style.background = 'transparent';
+	}
+	else{
+		document.querySelector('.menu').style.background = '#000';
+	}
+}
+
+
+function mostrar() {
+	document.querySelector('.header-account').style.display = 'flex';
+}
+
+function sumir() {
+	document.querySelector('.header-account').style.display = 'none';
+}
+
+
 function disparar () {
 	if (document.querySelector('input').style.display == 'flex'){
 	  document.querySelector('input').style.display = 'none';
 	  document.querySelector('span').style.border = 'none';
-	  document.querySelector ('.menu-2').style.width = '20vw'
-	  
-	} else {
+	  document.querySelector ('.menu-2').style.width = '20vw'  
+	} 
+	else {
 	  document.querySelector('input').style.display = 'flex';
 	  document.querySelector('span').style.border = '0.5px solid #FFFFFF';
 	  document.querySelector ('.menu-2').style.width = 'calc(20vw + 177px)'
